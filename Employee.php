dbfa7d11d67a22390cdcd5748bdddee026d7a3db
Ryan Kane <rmkane89@gmail.com>
2016-12-01 09:21:49 -0500

    Refactoring PHP example

--- Employee.php
+++ Employee.php
@@ -1,46 +1,49 @@
 <?php
 class Employee {
-  var $name;
-  var $salary;
-  
-  public function getName() {
-    return $this->name;
-  }
-  
-  protected function setName($name) {
-    $this->name = (string)$name;
-  }
-  
-  public function getSalary() {
-    return $this->salary;
-  }
-  
-  protected function setSalary($salary) {
-    $this->salary = floatval($salary);
-  }
+    var $name;
+    var $salary;
 
-  public function __construct() {
-    $numargs = func_num_args();
-    if ($numargs < 2 || $numargs > 2) {
-      $this->setName('');
-      $this->setSalary(0);
-    } else /* Overload */ {
-      $this->setName(func_get_arg(0));
-      $this->setSalary(func_get_arg(1));
+    public function getName() {
+        return $this->name;
     }
-  }
 
-  public function displayEmployee() {
-    printf("Name: %s, Salary: $%.2f\n", $this->getName(), $this->getSalary());
-  }
+    protected function setName($name) {
+        $this->name = (string)$name;
+    }
+
+    public function getSalary() {
+        return $this->salary;
+    }
+
+    protected function setSalary($salary) {
+        $this->salary = floatval($salary);
+    }
+
+    public function __construct() {
+        $numargs = func_num_args();
+        if ($numargs < 2 || $numargs > 2) {
+            $this->setName('');
+            $this->setSalary(0);
+        } else /* Overload */ {
+            $this->setName(func_get_arg(0));
+            $this->setSalary(func_get_arg(1));
+        }
+    }
+
+    public function __toString() {
+        return sprintf("Name: %s, Salary: $%.2f", $this->getName(), $this->getSalary());
+    }
+
+    public function displayEmployee() {
+		print(nl2br($this.PHP_EOL));
+    }
 }
 
 function main() {
-  $emp1 = new Employee("Bob", 75000);
-  $emp2 = new Employee("Joe", 50000);
-  $emp1->displayEmployee();
-  $emp2->displayEmployee();
+    $emp1 = new Employee("Bob", 75000);
+    $emp2 = new Employee("Joe", 50000);
+    $emp1->displayEmployee();
+    $emp2->displayEmployee();
 }
 
 main();
-?>
